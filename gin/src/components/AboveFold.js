6e8db569f6ff18e9800a37099e32b4e608b62c70
George Kolev <gogo1520180@gmail.com>
2021-10-31 19:30:31 +0200

    dev: what the fuck am i doing

---
+++ gin/src/components/AboveFold.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import PropTypes, { func } from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import "./abv-fold.scss"
+
+const AboveFold = () => {
+  return (
+    <div>
+      <div className="full-size"></div>
+    </div>
+  )
+}
+
+export default AboveFold
